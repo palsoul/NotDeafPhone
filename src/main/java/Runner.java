4f457eebba4f590bb902f4ec158aadf2280c5160
palsoul <hypra23@gmail.com>
2017-02-14 19:29:13 +0600

    Version 1.0.0

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,9 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @author: Brotherdos aka Janbrodos
  * @date: 14.02.2017
  */
 public class Runner {
     public static void main(String[] args) {
-
+        int number;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            number = reader.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        System.out.println(number);
     }
 }
